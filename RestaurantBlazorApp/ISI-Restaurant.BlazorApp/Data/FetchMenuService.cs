b01d645aeaf31a8f4302714219fdeb2fb204f0bd
Agata Jasionowska <jasionowska.agata@gmail.com>
2020-04-20 15:43:55 +0200

    BlazorApp - initial commit

---
+++ RestaurantBlazorApp/ISI-Restaurant.BlazorApp/Data/FetchMenuService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISI_Restaurant.BlazorApp.Data
+{
+    public class FetchMenuService
+    {
+    }
+}
---
+++ RestaurantBlazorApp/ISI-Restaurant.HttpClient/APIClient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace ISI_Restaurant.HttpClient
+{
+    public class APIClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly ILogger logger;
+        private readonly Uri baseUri;
+
+        public APIClient(HttpClient httpClient, ILogger<APIClient> logger)
+        {
+            this.httpClient = httpClient;
+            this.logger = logger;
+            var uriBuilder = new UriBuilder
+            {
+                Scheme = "http",
+                Host = "localhost",
+                Port = 8983
+            };
+            baseUri = uriBuilder.Uri;
+        }
+
+    }
+}
